b1c3b51f3abb9a2fe6f1bf18bbd2930ccfe6504f
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-09-09 03:18:07 +0600

    update dashboard and project page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import DashboardPage from "./projects/page"
 
 export default function Home() {
   return (
-   <div>
-    <ProjectsDashboardPage/>
+   <div> 
+    <DashboardPage/>
+    
    </div>
   );
 }
